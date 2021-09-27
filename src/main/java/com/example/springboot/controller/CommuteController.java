18f3c9f4a505d9a3714cd3e3c560a2b4b4e9dd4f
Jasper Rigters <jasper@Jaspers-MBP.trifork.nl>
2021-09-27 14:17:37 +0200

    Fails on the user name

--- src/main/java/com/example/springboot/controller/CommuteController.java
+++ src/main/java/com/example/springboot/controller/CommuteController.java
@@ -1,11 +1,16 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.dto.AddCommuteCommand;
+import com.example.springboot.model.Commute;
 import com.example.springboot.model.User;
 import com.example.springboot.repository.CommuteRepository;
+import com.example.springboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/commutes")
@@ -13,6 +18,8 @@ public class CommuteController {
 
     @Autowired
     private CommuteRepository repository;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
     public String index(Model model) {
@@ -20,13 +27,19 @@ public class CommuteController {
         model.addAttribute("commutes", iter);
         return "commutes";
     }
-    /*
-    @PostMapping("/")
-    public String postIndex(@ModelAttribute User user) {
-        repository.save(user);
+
+    @PostMapping()
+    public String postIndex(@ModelAttribute AddCommuteCommand commuteCommand) {
+
+        Optional<User> user = userRepository.findById(commuteCommand.getUserId());
+        Commute commute = new Commute();
+        commute.setHome(commuteCommand.getHome());
+        commute.setWork(commuteCommand.getWork());
+        commute.setUser(user.get());
+        user.get().getCommutes().add(commute);
+        userRepository.save(user.get());
         return "redirect:/";
     }
-    */
 
 
 
