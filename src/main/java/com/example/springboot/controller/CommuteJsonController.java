fdb2f1ccba4796079796b489ccee30502c9f4654
JWA van Golverdinge Schut <jaron.van.golverdinge-schut@diymaxeda.com>
2021-09-30 15:54:05 +0200

    Adding explaination of how to step by step refactor into a curried function

--- src/main/java/com/example/springboot/controller/CommuteJsonController.java
+++ src/main/java/com/example/springboot/controller/CommuteJsonController.java
@@ -21,12 +21,35 @@ public class CommuteJsonController {
     private CommuteRepository commuteRepository;
 
     @GetMapping
-    public Commute[] getAllCommuteInJson(@RequestParam(value = "filter",required = false) String filter) {
+    public Commute[] getAllCommuteInJson(@RequestParam(value = "filter",required = false) String someStringValue) {
         Iterable<Commute> commutes = commuteRepository.findAll();
 
         List<Commute> result = new ArrayList();
         StreamSupport.stream(commutes.spliterator(),false)
-                .filter(createFilter(filter))
+                // The fully written out lambda
+                .filter((Commute commute) -> {
+                    if(someStringValue == null) {
+                        return true;
+                    }
+                    if(commute.getHome().toLowerCase(Locale.ROOT)
+                            .contains(someStringValue.toLowerCase(Locale.ROOT))) {
+                        return true;
+                    } else if(commute.getWork().toLowerCase(Locale.ROOT)
+                            .contains(someStringValue.toLowerCase(Locale.ROOT))) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                })
+                // The shorter lambda using the filter in Commute
+                .filter((Commute commute) -> {
+                    return Commute.filter(someStringValue, commute);
+                })
+                // The shortest lambda using filter in Commute
+                .filter(commute -> Commute.filter(someStringValue, commute))
+                // Using a function to return a function
+                .filter(createFilter(someStringValue))
+                // All "Predicate" functions accept 1 Commute parameter and return a boolean value
                 .forEach(result::add);
 
         return result.toArray(new Commute[result.size()]);
